44ea859eda3ac42bdca0e1027ea85d1f2ae49f53
Aziz Souabni <57048839+azizswebni@users.noreply.github.com>
2024-05-21 23:44:56 +0100

    Change select language placement & Contact sectio nresponsive

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,89 +1,85 @@
 "use client";
 
-import { useLangContext } from "@/context/languageContext";
 import dynamic from "next/dynamic";
-import langData from '../../langs/data.json';
 
 const MyMap = dynamic(() => import("../MyMap/MyMap"), {
   loading: () => <p>loading...</p>,
-  ssr: false
-})
+  ssr: false,
+});
 
 const Contact = () => {
-  const { langs, lang, setLang } = useLangContext();
   return (
     <section id="contact" className="overflow-hidden py-16 md:py-20 lg:py-28">
       <div className="container">
-        <div className="-mx-8 flex">
-          <div className="w-full px-4 lg:w-7/12 xl:w-8/12">
-            <div
-              className="mb-12 rounded-sm bg-white px-8 py-11 shadow-three dark:bg-gray-dark sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
-              data-wow-delay=".15s
+        <div className="flex w-full flex-wrap space-x-4 justify-center items-center">
+          <div
+            className="max-w-3xl mb-12 rounded-sm bg-white px-8 py-11 shadow-three dark:bg-gray-dark sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
+            data-wow-delay=".15s
               "
-            >
-              <h2 className="mb-3 text-2xl font-bold text-black dark:text-white sm:text-3xl lg:text-2xl xl:text-3xl">
-              {langData[lang].contact_t0}
-              </h2>
-              <p className="mb-12 text-base font-medium text-body-color">
-              {langData[lang].contact_d0}              </p>
-              <form>
-                <div className="-mx-4 flex flex-wrap">
-                  <div className="w-full px-4 md:w-1/2">
-                    <div className="mb-8">
-                      <label
-                        htmlFor="name"
-                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
-                      >
-                        {langData[lang].contact_nme}
-                      </label>
-                      <input
-                        type="text"
-                        placeholder={langData[lang].contact_nme_tt}
-                        className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
-                      />
-                    </div>
-                  </div>
-                  <div className="w-full px-4 md:w-1/2">
-                    <div className="mb-8">
-                      <label
-                        htmlFor="email"
-                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
-                      >
-                        {langData[lang].contact_eml}
-                      </label>
-                      <input
-                        type="email"
-                        placeholder={langData[lang].contact_eml_tt}
-                        className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
-                      />
-                    </div>
-                  </div>
-                  <div className="w-full px-4">
-                    <div className="mb-8">
-                      <label
-                        htmlFor="message"
-                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
-                      >
-                        {langData[lang].contact_msg}
-                      </label>
-                      <textarea
-                        name="message"
-                        rows={5}
-                        placeholder={langData[lang].contact_msg_tt}
-                        className="border-stroke w-full resize-none rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
-                      ></textarea>
-                    </div>
-                  </div>
-                  <div className="w-full px-4">
-                    <button className="rounded-sm bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark">
-                    {langData[lang].contact_cta}
-                    </button>
+          >
+            <h2 className="mb-3 text-2xl font-bold text-black dark:text-white sm:text-3xl lg:text-2xl xl:text-3xl">
+            {langData[lang].contact_t0}
+            </h2>
+            <p className="mb-12 text-base font-medium text-body-color">
+            {langData[lang].contact_d0}
+            </p>
+            <form>
+              <div className="-mx-4 flex flex-wrap">
+                <div className="w-full px-4 md:w-1/2">
+                  <div className="mb-8">
+                    <label
+                      htmlFor="name"
+                      className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                    >
+                      {langData[lang].contact_nme}
+                    </label>
+                    <input
+                      type="text"
+                      placeholder={langData[lang].contact_nme_tt}
+                      className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                    />
                   </div>
                 </div>
-              </form>
-            </div>
+                <div className="w-full px-4 md:w-1/2">
+                  <div className="mb-8">
+                    <label
+                      htmlFor="email"
+                      className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                    >
+                      {langData[lang].contact_eml}
+                    </label>
+                    <input
+                      type="email"
+                      placeholder={langData[lang].contact_eml_tt}
+                      className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                    />
+                  </div>
+                </div>
+                <div className="w-full px-4">
+                  <div className="mb-8">
+                    <label
+                      htmlFor="message"
+                      className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                    >
+                     {langData[lang].contact_msg}
+                    </label>
+                    <textarea
+                      name="message"
+                      rows={5}
+                      placeholder={langData[lang].contact_msg_tt}
+                      className="border-stroke w-full resize-none rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                    ></textarea>
+                  </div>
+                </div>
+                <div className="w-full px-4">
+                  <button className="rounded-sm bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark">
+                  {langData[lang].contact_cta}
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
-          <div className="w-full h-full px-4 lg:w-8/12 xl:w-6/12">
+          <div className="h-full w-full max-w-xl">
             <MyMap />
           </div>
         </div>
